dd89f2446aaecc012c3026aba6f8f79b83c79787
Shvang42 <82295292+Shvang42@users.noreply.github.com>
2022-07-01 14:49:13 -0400

    added canvas

---
+++ Boss Fight/index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+//making canvas visible as a white rectangle
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+//Need to create HTML image in order to use it
+const image = new Image()
+image.src = './img/BattleGrounds.png'
+
+c.drawImage(image)
