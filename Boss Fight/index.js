34c1c3570776b7083ab440be0638aa5d8b86b9d6
Shvang42 <82295292+Shvang42@users.noreply.github.com>
2022-07-01 16:41:30 -0400

    Update index.js

--- Boss Fight/index.js
+++ Boss Fight/index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d')
 
-canvas.width = 960
-canvas.height = 704
+canvas.width = 640
+canvas.height = 960
 
 //making canvas visible as a white rectangle
 c.fillStyle = 'white'
