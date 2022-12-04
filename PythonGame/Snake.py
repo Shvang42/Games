2cd7b9a093fa68744a5fab149ab9e4afc49e542e
Shvang42 <82295292+Shvang42@users.noreply.github.com>
2022-12-03 19:26:31 -0500

    Update Snake.py

--- PythonGame/Snake.py
+++ PythonGame/Snake.py
@@ -1,29 +1,41 @@
 import pygame
 import sys
+from pygame.math import Vector2
 
 white = (255, 255, 255)
+grass_green = (175, 215, 70)
+red = (255, 0, 0)
 
 # creating a class in order to generate the "fruit" the snake will eat
-class APPLE:
-    def __init__(self):
+class Fruit:
+    def __init__(self, color):
         self.x = 5
         self.y = 4
-
-        # create an x and y position
+        self.pos = Vector2(self.x, self.y)
+        self.col = color
+        
+        # create an x and y position and vector
+        
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, 
+                                 cell_size, cell_size)
+        pygame.draw.rect(screen, self.col, fruit_rect)
         # draw a square
 
-
+class Apple(Fruit):
+        def __init__(self, Color_Red):
+            super().__init__(Color_Red)
+             
 # initialize basic game components, such as the display screen, FPS,
 # and a test surface, along with the "grid" look for the game
 pygame.init()
 cell_size = 40
 cell_number = 20
-screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
+screen = pygame.display.set_mode((cell_size * cell_number, 
+                                  cell_size * cell_number))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100, 200))
 
-# colors for the display screen
-screen.fill((175, 215, 70))
+apple = Apple(red)
 
 # while loop allows the game to continuously run unless
 # the EXIT button on the window is hit
@@ -33,6 +45,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.fill((175, 215, 70))
+    screen.fill(grass_green)
+    apple.draw_fruit()
     pygame.display.update()
     clock.tick(30)
